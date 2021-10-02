91ef72b8f942cbcf4524de3d5b3ac77c6359bbe3
AleksMMM <gol8an@yandex.ru>
2021-10-02 16:48:58 +0300

    не запускаются тесты на junit

---
+++ src/main/java/Collection/AddElement.java
@@ -0,0 +1,25 @@
+package Collection;
+
+/*
+необходимо реализовать метод,
+который добавляет элемент в список и сообщает нам удалось это или нет, т.е. метод должен вернуть булево значение.
+ */
+
+import java.util.List;
+
+public class AddElement {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean addNewElement(List<String> list, String str) {
+
+        int size = list.size();
+        list.add(str);
+        if (size > list.size()) {
+            return true;
+        }
+        return false;
+    }
+
+}
